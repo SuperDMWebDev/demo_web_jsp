6528e6cbfdd6e81ebba62296c687b2b4d5c7c4b6
SuperDMWebDev <hadtnt71@gmail.com>
2022-11-09 11:13:56 +0700

    person list

--- src/main/java/com/trinhtien2212/demo_web_jsp/controller/MainController.java
+++ src/main/java/com/trinhtien2212/demo_web_jsp/controller/MainController.java
@@ -1,24 +1,18 @@
 package com.trinhtien2212.demo_web_jsp.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.trinhtien2212.demo_web_jsp.model.product;
 import com.trinhtien2212.demo_web_jsp.service.CustomerService;
 import com.trinhtien2212.demo_web_jsp.service.MenuService;
 import com.trinhtien2212.demo_web_jsp.service.ProductService;
 import com.trinhtien2212.demo_web_jsp.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("")
 public class MainController {
    
     // @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
@@ -30,6 +24,9 @@ public class MainController {
 
     @Autowired
     private ReviewService reviewService;
+
+    @Autowired
+    private CustomerService customerService;
   
     @GetMapping(value={"/","/home"})
     public String index(Model model) {
@@ -43,13 +40,12 @@ public class MainController {
         return "index";
     }
 
-    // @RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
-    // @GetMapping(value={"/persons"})
-    // public String viewPersonList(Model model) {
-    //     System.out.println("Dang vao person list");
-    //     System.out.println(customerService.findAll());
-    //     model.addAttribute("persons", customerService.findAll());
+     @GetMapping(value =  "/personList" )
+     public String viewPersonList(Model model) {
+         System.out.println("Dang vao person list");
+         System.out.println(customerService.findAll());
+         model.addAttribute("persons", customerService.findAll());
 
-    //     return "personList";
-    // }
+         return "personList";
+     }
 }
--- src/main/java/com/trinhtien2212/demo_web_jsp/dao/MenuRepository.java
+++ src/main/java/com/trinhtien2212/demo_web_jsp/dao/MenuRepository.java
@@ -3,7 +3,9 @@ package com.trinhtien2212.demo_web_jsp.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.trinhtien2212.demo_web_jsp.model.Menu;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long>{
     
 }
